8fd2f466765383c50c38ec4ef317cedce9e17003
Hansraj-GitHub <hraj2662001@gmail.com>
2024-08-15 10:08:18 +0530

    day1

---
+++ app.js
@@ -0,0 +1,20 @@
+        
+
+        const parent = React.createElement(
+            "div" , {id:"parent"},
+            [React.createElement("div",{id:"child"},[
+                React.createElement("h1", {} , "i am in h1 tag"),
+                React.createElement("h2", {} , "i am in h2 tag"),
+    
+    
+            ]),
+            React.createElement("div",{id:"child"},[
+                React.createElement("h1", {} , "i am in h1 tag"),
+                React.createElement("h2", {} , "i am in h2 tag"),
+    
+    
+            ])]
+
+        );
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(parent);
